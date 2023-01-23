e6e27c20727043d94b4a89fd46ef3f664af8fb37
G10rgi <giokorg2001@gmail.com>
2023-01-23 23:05:51 +0400

    updt10

--- script.js
+++ script.js
@@ -4,14 +4,6 @@ window.addEventListener ("scroll", function() {
 	header.classList.toggle ("sticky", window.scrollY > 0);
 });
 
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
-menu.onclick = () => {
-	menu.classList.toggle('bx-x');
-	navbar.classList.toggle('open');
-};
-
 window.onscroll = () => {
 	menu.classList.remove('bx-x');
 	navbar.classList.remove('open');
@@ -45,3 +37,11 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+	menu.classList.toggle('bx-x');
+	navbar.classList.toggle('open');
+};
